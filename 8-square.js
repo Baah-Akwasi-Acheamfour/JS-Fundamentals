952f9807565aeb9540ad8f1953ef8af9f2a0262f
Baah-Akwasi-Acheamfour <akwasiacheamfourbaah@gmail.com>
2025-05-25 12:40:37 +0000

    fixed the errors

--- 8-square.js
+++ 8-square.js
@@ -1,10 +1,9 @@
 const size = Number(process.argv[2]);
 
-if (isNaN(size)) {
+if (!Number.isInteger(size)) {
   console.log("Missing size");
 } else {
   for (let i = 0; i < size; i++) {
     console.log("X".repeat(size));
   }
 }
-g;
